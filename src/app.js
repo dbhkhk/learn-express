26aa7f63f9b1f14683d17368d6dd3ab51b8a46c4
Brody Chen <rufengch@gmail.com>
2016-07-04 22:10:05 -0700

    Create app

---
+++ src/app.js
@@ -0,0 +1,13 @@
+'use strice';
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>');
+});
+
+app.listen(3000, () => {
+  console.log('The frontend server is running on port 3000.');
+});
